16bb5e75bfd85e54bf3757c2ea26004820e4cc2f
peterysc <peterysc@gmail.com>
2017-04-08 17:06:53 -0700

    cors

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ app.use(morgan('dev'));
 
 
 /* Handle each route */
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
